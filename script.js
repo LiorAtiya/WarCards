ffb6e46d379f40bfd76441d6ae0d0681ae047229
lior4007 <lior4007@gmail.com>
2019-10-19 20:44:58 +0300

    version 1.0

--- script.js
+++ script.js
@@ -4,27 +4,56 @@ var scoreA = 0;
 var scoreB = 0;
 
 function randomA(){
-    cardA = Math.ceil(Math.random()*4) // מעגל כלפי מעלה | floor - כלפי מטה 
-    placeA.src = "img/card"+cardA+".gif"
+    cardA = Math.ceil(Math.random()*13) // מעגל כלפי מעלה | floor - כלפי מטה 
+    placeA.src = "img/card"+cardA+".png"
+    
+    
 }
 
 function randomB(){
-    cardB = Math.ceil(Math.random()*4) // מעגל כלפי מעלה | floor - כלפי מטה 
-    placeB.src = "img/card"+cardB+".gif"
+    cardB = Math.ceil(Math.random()*13) // מעגל כלפי מעלה | floor - כלפי מטה 
+    placeB.src = "img/card"+cardB+".png"
 }
 
 function checkWin(){
-    if(cardA > cardB){
-        alert("A Win")
+    if(Number(cardA) < Number(cardB)){
+        alert("שחקן 1 ניצח")
         scoreA++;
     }
-    if(cardA < cardB){
-        alert("B Win")
+    if(Number(cardA) > Number(cardB)){
+        alert("שחקן 2 ניצח")
         scoreB++;
     }
-    if(cardA == cardB){
-        alert("Draw")
+    if(Number(cardA) === Number(cardB)){
+        alert("שיוויון")
     }
+    placeA.src = "";
+    placeB.src = "";
+    cardA = 0;
+    cardB = 0;
     player1.innerHTML = scoreA;
     player2.innerHTML = scoreB;
 }
+
+// ---------------------------------------
+
+// function checkWin2(a,b){
+//     switch (a,b){
+//         case (a < b):
+//             alert("שחקן 1 ניצח")
+//             scoreA++;
+//         break;
+//         case (a > b):
+//             alert("שחקן 2 ניצח")
+//             scoreB++;
+//         break;
+//         case (a === b):
+//             alert("שיוויון")
+//     }
+//     placeA.src = "";
+//     placeB.src = "";
+//     cardA = 0;
+//     cardB = 0;
+//     player1.innerHTML = scoreA;
+//     player2.innerHTML = scoreB;
+// }
